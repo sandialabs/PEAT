530f1ca503858303945eff96ea6d8feea492d00c
Christopher Goes <cegoes@sandia.gov>
2026-02-06 18:50:03 -0700

    docs: add GitHub logo to footer, add edit button

--- docs/conf.py
+++ docs/conf.py
@@ -58,9 +58,9 @@ autodoc_pydantic_model_show_json = True
 
 # -- Project metadata --------------------------------------------------------
 project = "PEAT"
-current_year = datetime.now().year
+language = "en"
 date = datetime.now().strftime("%m/%d/%Y")
-copyright = f"2016 - {current_year}, Sandia National Laboratories"
+copyright = f"2016 - {datetime.now().year}, Sandia National Laboratories"
 author = "Sandia National Laboratories"
 authors = sorted(_clean_read(Path(pardir, "AUTHORS")))
 version = get_git_version()
@@ -80,7 +80,6 @@ templates_path = ["_templates"]
 
 # The “master” document, contains the root toctree directive
 master_doc = "index"
-language = "en"
 exclude_patterns = [
     "_build",
     "_built_docs",
@@ -150,6 +149,24 @@ html_short_title = "PEAT"
 html_favicon = "favicon.ico"
 html_logo = "favicon.ico"
 html_last_updated_fmt = ""
+html_theme_options = {
+    "top_of_page_buttons": ["view", "edit"],
+    "source_repository": "https://github.com/sandialabs/PEAT/",
+    "source_branch": "main",
+    "source_directory": "docs/",
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/sandialabs/PEAT",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
+}
 
 
 # Build command line interface (CLI) manpage (e.g. "man peat.1")
